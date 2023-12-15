24dbc59ef8f2e76a30d0deb53ede42c8cb31cd14
Bodoi <109707645+kieuduc123@users.noreply.github.com>
2023-12-15 20:56:48 +0700

    design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import Footer from "utils/Footer";
+import Header from "utils/Header";
+
+const HomePage = React.lazy(() => import("components/page/HomePage"));
+const About = React.lazy(() =>  import("components/page/About"));
+const Work = React.lazy(() => import("components/page/Work"));
+const Work_Single = React.lazy(() => import("components/page/Work_Single"));
+const Contact = React.lazy(() => import ("components/page/Contact"))
+const Fallback = () => {
+  setTimeout(()=> {
+
+  },2000)
+  return <div>Loading.......</div>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Fragment>
+    <Header/>
+    <Suspense fallback= {<Fallback/>}> 
+   <Routes>
+    <Route path="/" element={<HomePage/>}></Route>
+    <Route path="/about" element={<About/>}></Route>
+    <Route path="/work" element={<Work/>}></Route>
+    <Route path="/work/:id" element={<Work_Single/>}></Route>
+    <Route path="/contact" element={<Contact/>}></Route>
+
+   </Routes>
+   </Suspense>
+
+   <Footer/>
+   </Fragment>
   );
 }
 
